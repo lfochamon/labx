0b23644c4d9c0602d7a3346ce35fb16199a0696b
Luiz Chamon <luizchamon@gmail.com>
2025-02-20 09:26:18 +0100

    bug fix
    Signed-off-by: Luiz Chamon <luizchamon@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Material for labX",
     author="Luiz Chamon",
     url="https://github.com/lfochamon/labx",
-    packages="labx",
+    packages=["labx"],
     include_package_data=True,
     install_requires=["torch"],
 )
