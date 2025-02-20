b934fc3947add394d9c50313ce238560c29abc00
Luiz Chamon <luizchamon@gmail.com>
2025-02-20 09:17:20 +0100

    first commit
    Signed-off-by: Luiz Chamon <luizchamon@gmail.com>

---
+++ labx/resnet18.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import os
+
+
+class BasicBlock(nn.Module):
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        stride=1,
+        downsample=None,
+        norm_layer=None,
+    ):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.conv1 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn1 = norm_layer(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, padding=1, bias=False)
+        self.bn2 = norm_layer(out_planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(
+        self,
+        num_classes=10,
+    ):
+        super(ResNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 64, stride=1)
+        self.layer2 = self._make_layer(64, 128, stride=2)
+        self.layer3 = self._make_layer(128, 256, stride=2)
+        self.layer4 = self._make_layer(256, 512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, in_planes, out_planes, stride=1):
+        downsample = None
+        if in_planes != out_planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(out_planes),
+            )
+
+        return nn.Sequential(
+            BasicBlock(
+                in_planes=in_planes,
+                out_planes=out_planes,
+                stride=stride,
+                downsample=downsample,
+                norm_layer=nn.BatchNorm2d,
+            ),
+            BasicBlock(
+                in_planes=out_planes,
+                out_planes=out_planes,
+                norm_layer=nn.BatchNorm2d,
+            ),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def resnet18(pretrained, device):
+    model = ResNet()
+    if pretrained:
+        script_dir = os.path.dirname(__file__)
+        state_dict = torch.load(script_dir + "/state_dicts/resnet18.pt", weights_only=True)
+        model.load_state_dict(state_dict)
+    return model.to(device)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="labx",
+    version="0.1",
+    description="Material for labX",
+    author="Luiz Chamon",
+    url="https://github.com/lfochamon/labx",
+    packages='labx',
+    include_package_data=True,
+    install_requires=["torch"],
+)
+
